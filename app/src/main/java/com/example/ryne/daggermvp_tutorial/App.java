c14d420c6c9debaa4138573a3207cbd9817963da
rgreen88 <rynelgreen@gmail.com>
2018-01-09 19:20:35 -0500

    added DaggerNetComponent.builder() and returns mNetComponent. Rebuilding doesn't clear Dagger error

--- app/src/main/java/com/example/ryne/daggermvp_tutorial/App.java
+++ app/src/main/java/com/example/ryne/daggermvp_tutorial/App.java
@@ -2,16 +2,34 @@ package com.example.ryne.daggermvp_tutorial;
 
 import android.app.Application;
 
+
+import com.example.ryne.daggermvp_tutorial.data.component.NetComponent;
+import com.example.ryne.daggermvp_tutorial.data.module.AppModule;
+import com.example.ryne.daggermvp_tutorial.data.module.NetModule;
+
 /**
  * Created by Ryne on 1/9/2018.
  */
 
 public class App extends Application {
 
+    private NetComponent mNetComponent;
+
     //Overriding default Application class, so we need to modify application name to launch App
     @Override
     public void onCreate(){
         super.onCreate();
+
+        //creating dagger builder object from NetComponent object
+        mNetComponent = DaggerNetComponent.builder()
+                .appModule(new AppModule(this))
+                .netModule(new NetModule("http://jsonplaceholder.typicode.com/"))
+                .build();
+
+    }
+
+    public NetComponent getNetComponent() {
+        return mNetComponent;
     }
 
 }
